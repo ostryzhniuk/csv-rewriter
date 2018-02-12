ad0af0b7cdf53f71614c91451c8499ef7b061986
Andrii Ostryzhniuk <andrii.ostryzhniuk@sombrainc.com>
2018-02-12 12:40:31 +0200

    Rewrite tests for writing csv

--- src/main/java/com/sombrainc/Application.java
+++ src/main/java/com/sombrainc/Application.java
@@ -1,10 +1,9 @@
 package com.sombrainc;
 
-import com.sombrainc.dto.sap.CompaniesDTO;
+import com.sombrainc.dto.sap.CompanyDTO;
 import com.sombrainc.utils.CSVHandler;
 
 import java.io.File;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class Application {
@@ -12,9 +11,9 @@ public class Application {
     public static void main(String[] args) {
 
         File companiesCsv = new File("/home/sombra-48/Desktop/OCRDMAIN.csv");
-        List<CompaniesDTO> companies = CSVHandler.parseBeans(companiesCsv, CompaniesDTO.class);
+        List<CompanyDTO> companies = CSVHandler.parseBeans(companiesCsv, CompanyDTO.class);
 
-        CSVHandler.writeBeans(companies, Paths.get("/home/sombra-48/Desktop/companies.csv"), CompaniesDTO.class);
+        CSVHandler.writeBeans(companies, new File("/home/sombra-48/Desktop/companies.csv"), CompanyDTO.class);
 
      }
 
