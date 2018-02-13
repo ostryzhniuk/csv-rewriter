821071184a19be245ad401447fca9d26fce49958
Andrii Ostryzhniuk <andrii.ostryzhniuk@sombrainc.com>
2018-02-13 18:38:03 +0200

    Use projectlombok @Data annotation for CompanyContactDTO

--- src/main/java/com/sombrainc/dto/sap/CompanyContactDTO.java
+++ src/main/java/com/sombrainc/dto/sap/CompanyContactDTO.java
@@ -1,9 +1,11 @@
 package com.sombrainc.dto.sap;
 
 import com.univocity.parsers.annotations.Parsed;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 public class CompanyContactDTO implements Serializable {
 
     @Parsed(index = 0)
@@ -27,86 +29,4 @@ public class CompanyContactDTO implements Serializable {
     @Parsed(index = 9)
     private String email;
 
-    public CompanyContactDTO() {
-    }
-
-    public String getCardCode() {
-        return cardCode;
-    }
-
-    public void setCardCode(String cardCode) {
-        this.cardCode = cardCode;
-    }
-
-    public String getLineNum() {
-        return lineNum;
-    }
-
-    public void setLineNum(String lineNum) {
-        this.lineNum = lineNum;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getMobilePhone() {
-        return mobilePhone;
-    }
-
-    public void setMobilePhone(String mobilePhone) {
-        this.mobilePhone = mobilePhone;
-    }
-
-    public String getFax() {
-        return fax;
-    }
-
-    public void setFax(String fax) {
-        this.fax = fax;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
