3ba55c8f9c6b8a01661afb93c6ae52d54e0c15ef
Andrii Ostryzhniuk <andrii.ostryzhniuk@sombrainc.com>
2018-02-13 18:37:27 +0200

    Use projectlombok @Data annotation for CompanyAddressDTO

--- src/main/java/com/sombrainc/dto/sap/CompanyAddressDTO.java
+++ src/main/java/com/sombrainc/dto/sap/CompanyAddressDTO.java
@@ -1,9 +1,11 @@
 package com.sombrainc.dto.sap;
 
 import com.univocity.parsers.annotations.Parsed;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 public class CompanyAddressDTO implements Serializable {
 
     @Parsed(index = 0)
@@ -27,86 +29,4 @@ public class CompanyAddressDTO implements Serializable {
     @Parsed(index = 9)
     private String addressType;
 
-    public CompanyAddressDTO() {
-    }
-
-    public String getCardCode() {
-        return cardCode;
-    }
-
-    public void setCardCode(String cardCode) {
-        this.cardCode = cardCode;
-    }
-
-    public String getLineNum() {
-        return lineNum;
-    }
-
-    public void setLineNum(String lineNum) {
-        this.lineNum = lineNum;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddressName() {
-        return addressName;
-    }
-
-    public void setAddressName(String addressName) {
-        this.addressName = addressName;
-    }
-
-    public String getAddressName2() {
-        return addressName2;
-    }
-
-    public void setAddressName2(String addressName2) {
-        this.addressName2 = addressName2;
-    }
-
-    public String getAddressName3() {
-        return addressName3;
-    }
-
-    public void setAddressName3(String addressName3) {
-        this.addressName3 = addressName3;
-    }
-
-    public String getAddressType() {
-        return addressType;
-    }
-
-    public void setAddressType(String addressType) {
-        this.addressType = addressType;
-    }
 }
